004ecbfd41d7038887169cb0065daaac6176181f
Teya <teyaevans17@gmail.com>
2023-03-06 00:29:58 -0500

    questionnaires added to path

--- src/pages/journey/Journey.tsx
+++ src/pages/journey/Journey.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
-import { Button, Col, FlexboxGrid, Grid, IconButton, Panel, Row } from "rsuite";
+import { Button, Col, FlexboxGrid, Grid, IconButton, Modal, Panel, Row } from "rsuite";
 import { MDXProvider } from "@mdx-js/react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { chapterParts } from "@/pages/textbook/chapters/chapters";
@@ -8,6 +8,8 @@ import PageContent from "@/components/PageContent";
 import ReactMarkdown from "react-markdown";
 import ArrowBackIcon from '@rsuite/icons/ArowBack';
 import { unary } from "lodash";
+import LineTo from 'react-lineto';
+import { MultipleChoiceQuestion } from "./components/MultipleChoiceQuestion";
 
 export const Journey = observer(() => {
 
@@ -36,14 +38,28 @@ export const Journey = observer(() => {
 
     const [placeholdPath1, setPlaceholdPath1] = React.useState(false);
     const [placeholdPath2, setPlaceholdPath2] = React.useState(false);
- 
+
+    const [open1, setOpen1] = React.useState(false);
+    const handleOpen1 = () => setOpen1(true);
+
+    const [open2, setOpen2] = React.useState(false);
+    const handleOpen2 = () => setOpen2(true);
+
+    const [open3, setOpen3] = React.useState(false);
+    const handleOpen3 = () => setOpen3(true);
+    
+    const handleClose = () => 
+    {setOpen1(false); 
+    setOpen2(false); 
+    setOpen3(false);}
+
+    const [isClicked1, setIsClicked1] = React.useState(false);
+    const [isClicked2, setIsClicked2] = React.useState(false);
+    const [isClicked3, setIsClicked3] = React.useState(false);
+
 
     const i = 0;
 
-    //const handleOpen = () => setOpen(true);
-    //const handleClose = () => setOpen(false);
-
-    //const available = 
 
     const partToRender = chapterParts.find((part) => part.id === chapter);
 
@@ -52,7 +68,7 @@ export const Journey = observer(() => {
             <IconButton
                 className="back-button"
                 icon={<ArrowBackIcon />}
-                onClick={() => { navigate('/textbook') }}
+                onClick={() => { navigate('/course_apts') }}
             >
             </IconButton>
         );
@@ -70,7 +86,7 @@ export const Journey = observer(() => {
 
         return (
             <Button
-                disabled={disableButton()}
+                //disabled={disableButton()}
                 className={`path-option ${isUnavailable ? "unavailable" : "part-of-path"} `}
                 onClick={handleOnClick}>
 
@@ -79,11 +95,11 @@ export const Journey = observer(() => {
     });
 
     //const disableButton = () => {
-        //if (!isUnavailable) {
-        //    return true;
-        //} else {
-        //    return false;
-        //}
+    //if (!isUnavailable) {
+    //    return true;
+    //} else {
+    //    return false;
+    //}
     //}
 
 
@@ -112,7 +128,7 @@ export const Journey = observer(() => {
     };
 
     const handlePath1Click = () => {
-        
+
         setIsPath1(true);
         setIsPath2(false);
         setIsPath3(false);
@@ -133,7 +149,7 @@ export const Journey = observer(() => {
         setPlaceholdPath1(true);
     };
 
-    
+
     const handlePath4Click = () => {
         setIsPath4(true);
         setIsPath5(false);
@@ -157,8 +173,29 @@ export const Journey = observer(() => {
         setPlaceholdPath2(true);
     };
 
+    const handleAnswerClick1 = () => {
+        setIsClicked1(true);
+        setIsClicked2(false);
+        setIsClicked3(false);
+    };
+    const handleAnswerClick2 = () => {
+        setIsClicked1(false);
+        setIsClicked2(true);
+        setIsClicked3(false);
+    };
+    const handleAnswerClick3 = () => {
+        setIsClicked1(false);
+        setIsClicked2(false);
+        setIsClicked3(true);
+    };
 
-    
+    const handleClear = () => {
+        setIsClicked1(false);
+        setIsClicked2(false);
+        setIsClicked3(false);
+    };
+
+
 
 
     return (
@@ -168,23 +205,55 @@ export const Journey = observer(() => {
                 <Panel
                     className="question-card"
                 >
-                    <Grid style={{ width: "100%"}} align = "center">
-                    <Row >
-                    <Col xs={24} md={8}>
+                    <Grid style={{ width: "100%" }} align="center">
+                        <Row >
+                            <Col xs={24} md={8}>
                             </Col>
                             <Col xs={24} md={8}>
                                 <Button
                                     size="lg"
-                                    className={`path-option ${placeholdPath1 ? "part-of-path" : isCurrent1 ? "current": "unavailable"} `}
-                                    onClick={handleQuiz1Click}>
-                                    </Button>
+                                    className={`path-option ${placeholdPath1 ? "part-of-path" : isCurrent1 ? "current" : "unavailable"} `}
+                                    //on click handquiz1click and open modal
+                                    onClick={() => { handleQuiz1Click(); handleOpen1() }}>
+
+                                </Button>
+                                <Modal autoFocus={true} open={open1} backdrop="static" size="lg" onClose={handleClose}
+                                    backdropClassName="quiz-modal-backdrop"
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+
+                                    }}>
+                                    <Modal.Header>
+                                        <Modal.Title><h2>Questionnaire</h2></Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <h4>Which of the following do you prefer?
+                                        </h4>
+                                    </Modal.Body>
+                                    <Modal.Body>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked1 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick1}> Blue </Button>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked2 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick2}> Red </Button>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked3 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick3}> Green </Button>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button size="lg" onClick={() => { handleClose(); handleClear() }}>Close </Button>
+                                    </Modal.Footer>
+                                </Modal>
                             </Col>
                             <Col xs={24} md={8}>
                             </Col>
                         </Row>
                         <Row>
                             <Col xs={24} md={8}>
-                                
+
                                 <Button
                                     className={`path-option ${placeholdQ2 && isPath1 ? "part-of-path" : isPath1 ? "current" : "unavailable"} `}
                                     onClick={handlePath1Click}></Button>
@@ -192,23 +261,55 @@ export const Journey = observer(() => {
                             <Col xs={24} md={8}>
 
                                 <Button
-                                className={`path-option ${placeholdQ2 && isPath2 ? "part-of-path" : isPath2 ? "current" : "unavailable"} `}
+                                    className={`path-option ${placeholdQ2 && isPath2 ? "part-of-path" : isPath2 ? "current" : "unavailable"} `}
                                     onClick={handlePath2Click}>
 
                                 </Button>
                             </Col><Col xs={24} md={8}>
-                            <Button
-                            className={`path-option ${placeholdQ2 && isPath3 ? "part-of-path" : isPath3 ? "current" : "unavailable"} `}
+                                <Button
+                                    className={`path-option ${placeholdQ2 && isPath3 ? "part-of-path" : isPath3 ? "current" : "unavailable"} `}
                                     onClick={handlePath3Click}> </Button>
                             </Col>
                         </Row>
                         <Row>
-                        <Col xs={24} md={8}>
+                            <Col xs={24} md={8}>
                             </Col>
                             <Col xs={24} md={8}>
-                            <Button
-                                    className={`path-option ${placeholdQ3 || placeholdPath2 ? "part-of-path" : isCurrent2  && placeholdPath1 ? "current" : "unavailable"} `}
-                                    onClick={handleQuiz2Click}></Button>
+                                <Button
+                                    className={`path-option ${placeholdQ3 || placeholdPath2 ? "part-of-path" : isCurrent2 && placeholdPath1 ? "current" : "unavailable"} `}
+                                    onClick={() => { handleQuiz2Click(); handleOpen2() }}>
+
+                                </Button>
+                                <Modal autoFocus={true} open={open2} backdrop="static" size="lg" onClose={handleClose}
+                                    backdropClassName="quiz-modal-backdrop"
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+
+                                    }}>
+                                    <Modal.Header>
+                                        <Modal.Title><h2>Questionnaire</h2></Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <h4>Which of the following do you prefer?
+                                        </h4>
+                                    </Modal.Body>
+                                    <Modal.Body>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked1 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick1}> Harry Potter </Button>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked2 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick2}> Spongebob </Button>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked3 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick3}> Bojack Horseman </Button>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button size="lg" onClick={() => { handleClose(); handleClear() }}>Close </Button>
+                                    </Modal.Footer>
+                                </Modal>
                             </Col>
                             <Col xs={24} md={8}>
                             </Col>
@@ -217,35 +318,68 @@ export const Journey = observer(() => {
                             <Col xs={24} md={8}>
 
                                 <Button
-                                className={`path-option ${placeholdQ3 && isPath4 ? "part-of-path" : isPath4 ? "current" : "unavailable"} `}
+                                    className={`path-option ${placeholdQ3 && isPath4 ? "part-of-path" : isPath4 ? "current" : "unavailable"} `}
                                     onClick={handlePath4Click}>
 
-                                    </Button>
+                                </Button>
                             </Col>
                             <Col xs={24} md={8}>
 
                                 <Button
-                                className={`path-option ${placeholdQ3 && isPath5 ? "part-of-path" : isPath5 ? "current" : "unavailable"} `}
-                                onClick={handlePath5Click}>
+                                    className={`path-option ${placeholdQ3 && isPath5 ? "part-of-path" : isPath5 ? "current" : "unavailable"} `}
+                                    onClick={handlePath5Click}>
 
                                 </Button>
                             </Col><Col xs={24} md={8}>
 
-                            <Button
-                            className={`path-option ${placeholdQ3 && isPath6 ? "part-of-path" : isPath6 ? "current" : "unavailable"} `}
+                                <Button
+                                    className={`path-option ${placeholdQ3 && isPath6 ? "part-of-path" : isPath6 ? "current" : "unavailable"} `}
                                     onClick={handlePath6Click}> </Button>
                             </Col>
                         </Row>
                         <Row>
-                        <Col xs={24} md={8}>
+                            <Col xs={24} md={8}>
                             </Col>
                             <Col xs={24} md={8}>
-                            <Button
+                                <Button
                                     className={`path-option ${isCurrent3 ? "current" : "unavailable"} `}
-                                    onClick={handleQuiz3Click}></Button>
+                                    onClick={() => { handleQuiz3Click(); handleOpen3() }}>
+
+                                </Button>
+                                <Modal autoFocus={true} open={open3} backdrop="static" size="lg" onClose={handleClose}
+                                    backdropClassName="quiz-modal-backdrop"
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+
+                                    }}>
+                                    <Modal.Header>
+                                        <Modal.Title><h2>Questionnaire</h2></Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <h4>Which of the following do you prefer?
+                                        </h4>
+                                    </Modal.Body>
+                                    <Modal.Body>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked1 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick1}> The Ocean </Button>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked2 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick2}> The City </Button>
+                                        <Button size="md"
+                                            className={`path-option ${isClicked3 ? "current" : "part-of-path"} `}
+                                            onClick={handleAnswerClick3}> The Country </Button>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button size="lg" onClick={() => { handleClose(); handleClear() }}>Close </Button>
+                                    </Modal.Footer>
+                                </Modal>
+
                             </Col>
                             <Col xs={24} md={8}>
-                                
+
                             </Col>
                         </Row>
                     </Grid>
