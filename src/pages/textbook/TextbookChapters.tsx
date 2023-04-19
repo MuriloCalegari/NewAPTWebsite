08797d499ba0c751cd3606149f2559a1efc5e54e
Annie Sawers <anniesawers@Annies-MacBook-Air-2.local>
2023-04-19 16:47:49 -0400

    added back og pages

--- src/pages/textbook/TextbookChapters.tsx
+++ src/pages/textbook/TextbookChapters.tsx
@@ -1,43 +1,65 @@
 import React from "react";
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import PageContent from "@/components/PageContent";
-import {Panel, PanelGroup, Progress} from "rsuite";
-import {List, ListItem, ListItemButton, ListItemText} from "@mui/material";
+import { Panel, PanelGroup, Progress } from "rsuite";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import {chapters, Part} from "@/pages/textbook/chapters/chapters";
+import { chapters, Part } from "@/pages/textbook/chapters/chapters";
 
 export const TextbookChapters = observer(() => {
 
     const navigate = useNavigate();
 
     function goToPart(part: Part) {
-        navigate(`/textbook/${part.id}`);
+        navigate(`/content/${part.id}`);
     }
 
-    return (<PageContent bodyFill className="textbook-page" header={<h3 className="title" style={{marginTop:20}}>Textbook Name</h3>}>
-        
+    return (<PageContent bodyFill className="textbook-page" header={<h3 className="title" style={{ marginTop: 20 }}>Textbook Name</h3>}>
+
         <Panel collapsible bordered className="section" header={
             <div>
-            <div> Assigned Readings </div>
-            <div> Required | Due Wednesday, May 9 | Complete all 4 of them</div>
-            <Progress.Line style={{ width:"100%" }} percent={75} showInfo={false} strokeColor={"var(--blue)"} />
+                <div> Assigned Readings </div>
+                <div> Required | Due Wednesday, May 9 | Complete all 4 of them</div>
+                <Progress.Line style={{ width: "100%" }} percent={75} showInfo={false} strokeColor={"var(--blue)"} />
             </div>}>
-            
-             <div>
-                <div> 
+
+            <div>
+                <div>
                     <Progress.Circle showInfo={false} className="circle" percent={100} />
-                        <div>Chapter 1</div>
+                    <div>Chapter 1</div>
                 </div>
                 <div>Chapter 2</div>
                 <div>Chapter 3</div>
-                
-                
-                
-                </div>       
+            </div>
 
         </Panel>
-        
-            
-        </PageContent>
+
+        <Panel collapsible bordered className="section">
+            {chapters.map((chapter, chapterIndex) => {
+                return (
+                    <Panel header={<h5>{(chapterIndex + 1) + ". " + chapter.title}</h5>} eventKey={chapter.id}>
+                        <List>
+                            {chapter.parts.map((part, index) => {
+                                return (
+                                    <ListItem>
+                                        <ListItemButton onClick={() => goToPart(part)}>
+                                            <ListItemText
+                                                primary={(chapterIndex + 1) + "." + (index + 1) + " " + part.title}
+                                            />
+                                        </ListItemButton>
+                                    </ListItem>
+                                );
+                            })
+                            }
+                        </List>
+                    </Panel>
+                );
+            })
+            }
+
+        </Panel>
+
+
+    </PageContent>
     );
 });
