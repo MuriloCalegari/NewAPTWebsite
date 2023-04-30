296ab60f0bc08b6fd3dbb8f614653173e35c34f7
Alice Hu <yh353@duke.edu>
2023-04-30 19:05:31 -0400

    highlights page

--- src/pages/highlights/Highlights.tsx
+++ src/pages/highlights/Highlights.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import {observer} from "mobx-react-lite";
-import { useNavigate } from "react-router-dom";
-
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { useNavigate } from 'react-router-dom';
+import { data } from './highlight-data';
+import './styles.less';
 
 export const Highlights = observer(() => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function goToContents() {
-        navigate(`/contents`);
-    }
+  function goToContents(location: string) {
+    navigate(location);
+  }
 
-    return(
-        <div className='highlights-container'>
-            <div> HI ALICE! </div>
-        </div>
-       
-
-
-    );
-})
+  return (
+    <div className="highlights-container">
+      <h1 className="highlights-title">Highlights:</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index} className="highlight-item">
+            <p className="highlight-text">"{item.content}"</p>
+            <button onClick={() => { goToContents(item.location); }}>Go there.</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});
---
+++ src/pages/highlights/highlight-data.ts
@@ -0,0 +1,28 @@
+export const data = [{
+    "content": "Last In First Out (LIFO) principle",
+    "location": "/contents/page/stacks"
+},
+{
+    "content": "The length of a sentence is the number of words in it. See the examples for details.",
+    "location": "/contents/page/lists_warmup"
+},
+{
+    "content": "Tree data structure ... each node has at most two childrenblah blah",
+    "location": "/contents/page/trees"
+},
+{
+    "content": "left child of the root node has a value of 2",
+    "location": "/contents/page/trees2"
+},
+{
+    "content": "O(n) in the worst case",
+    "location": "/contents/page/stacks"
+},
+{
+    "content": "Note: The stack should be implemented using an array and the operations should be implemented using array methods.",
+    "location": "/contents/page/stacks1"
+},
+{
+    "content": "A data structure that follows a last-in, first-out (LIFO) order",
+    "location": "/contents/page/stacks1"
+}]
