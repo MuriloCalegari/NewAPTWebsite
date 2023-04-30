5f135307a43ccc09e67b81d3f64e2941eaae6e06
nerdyTJ <trinity.dy.johnson3@gmail.com>
2023-04-30 19:44:32 -0400

    Updated Parsons
    *Integrated Parsons problems on Style Guide Page
    *Added a stylesheet for Parsons component
    *changed parsons extension to tsx

--- src/pages/styleguide/StyleGuide.tsx
+++ src/pages/styleguide/StyleGuide.tsx
@@ -3,7 +3,23 @@ import {observer} from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
 // @ts-ignore
 import gif from "@/media/animations/ListSort.gif";
+import {Parsons} from "@/components/knowledge_tests/parsons";
 
+const data = [
+    {
+      question: "Question Prompt",
+      code: [
+        { text: "Roses are red", position: 0 },
+        { text: "Violets are blue", position: -1 },
+        { text: "Sugar is sweet", position: 1 },
+        { text: "And so are you", position: -1 }
+      ],
+      answerLength: function(){
+        let exclude = (this.code).filter(line => line.position === -1);
+        return (this.code).length - exclude.length
+      }
+  }
+  ];
 
 export const StyleGuide = observer(() => {
     const navigate = useNavigate();
@@ -40,7 +56,7 @@ export const StyleGuide = observer(() => {
         <div>Check your understanding</div>
 
         <div>parsons problems</div>
-
+            <Parsons/>
 
         </div>
 
